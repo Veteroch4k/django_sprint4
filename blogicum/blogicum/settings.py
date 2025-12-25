254b8a7e5701154a7c0009be3211b43d3c81bd62
Veteroch4k <indefinite05@mail.ru>
2025-12-25 21:27:06 +0700

    ref: forgot to change DEBUG to True

--- blogicum/blogicum/settings.py
+++ blogicum/blogicum/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1ficuc%zomb1jf2%3ks=+s+20e3andgedaa-wy0u@ioga9dog7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
